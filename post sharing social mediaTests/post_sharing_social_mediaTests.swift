8e33740b67587c80ba0360a186a00085a436a59b
Vadym Bogdan <vadymbog@gmail.com>
2020-06-24 12:01:39 +0300

    Initial Commit

---
+++ post sharing social mediaTests/post_sharing_social_mediaTests.swift
@@ -0,0 +1,34 @@
+//
+//  post_sharing_social_mediaTests.swift
+//  post sharing social mediaTests
+//
+//  Created by Вадим on 24.06.2020.
+//  Copyright © 2020 Vadym Bogdan. All rights reserved.
+//
+
+import XCTest
+@testable import post_sharing_social_media
+
+class post_sharing_social_mediaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
